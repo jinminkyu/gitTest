fe8bc5ab3f69c893946f936930e6b8949c6d7bfe
mkjin <rjswmtk@gmail.com>
2019-01-29 17:11:06 +0900

    충돌해결

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
 public class Main{
 	private String mainName;
-<<<<<<< HEAD
 	private String price;	//이번에 추가되었습니다.
 	// OtherUser가 수정
-=======
-	//gitOtherUser add comment
->>>>>>> 2d6fdae9ebf12a2c4ce89895707aec67954efa58
 }
